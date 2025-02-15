a7aef9e18c15ed0a0865b702feee334df1e6c460
SiriosDev <26876994+SiriosDev@users.noreply.github.com>
2025-02-15 18:39:16 +0100

    update misc.xml, pom.xml and module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,8 @@ module it.unicalingsw.issuereportcorpattern {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires org.controlsfx.controls;
 
     opens it.unicalingsw.issuereportcorpattern to javafx.fxml;
     exports it.unicalingsw.issuereportcorpattern;
-    exports it.unicalingsw.issuereportcorpattern.controller;
-    opens it.unicalingsw.issuereportcorpattern.controller to javafx.fxml;
 }
