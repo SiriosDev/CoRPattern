5aee62b29154a4742a01f2c9a918ab41ec43ac79
SiriosDev <26876994+SiriosDev@users.noreply.github.com>
2025-02-15 18:13:36 +0100

    initial

---
+++ src/main/java/it/unicalingsw/issuereportcorpattern/controller/IssuesScreen.java
@@ -0,0 +1,14 @@
+package it.unicalingsw.issuereportcorpattern.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IssuesScreen implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/main/java/it/unicalingsw/issuereportcorpattern/controller/ReportScreen.java
@@ -0,0 +1,14 @@
+package it.unicalingsw.issuereportcorpattern.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReportScreen  implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
